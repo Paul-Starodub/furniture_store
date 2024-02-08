74292b25ffb53121a86caad0f8f4a47bf1a4063a
Paul Starodub <amorallex@gmail.com>
2024-02-08 12:16:23 +0200

    add index.html, update views.py

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Home Page")
+    context = {"title": "Home", "content": "Welcome to Home"}
+    return render(request, "main/index.html", context)
 
 
 def about(request: HttpRequest) -> HttpResponse:
