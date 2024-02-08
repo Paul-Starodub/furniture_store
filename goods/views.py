f5e2e86bf67e348663a3e164216fe09feb423c67
Paul Starodub <amorallex@gmail.com>
2024-02-08 22:24:46 +0200

    add catalog.html & product.html, update base.html & urls.py & views.py

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def catalog(request: HttpRequest) -> HttpResponse:
+    return render(request, "goods/catalog.html")
+
+
+def product(request: HttpRequest) -> HttpResponse:
+    return render(request, "goods/product.html")
