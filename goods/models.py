1fb47fd0f9da9270529bc6bcac28139e7d543666
Paul Starodub <amorallex@gmail.com>
2024-02-09 14:57:45 +0200

    update models.py with verbose_name for slug

--- goods/models.py
+++ goods/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
 
     class Meta:
         db_table = "category"
