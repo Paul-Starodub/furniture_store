4777236c79bb2c4655d19a86470ac96c635261df
Paul Starodub <amorallex@gmail.com>
2024-02-09 23:01:58 +0200

    update goods/models.py with model `products` and register it, add migrations

--- goods/models.py
+++ goods/models.py
@@ -11,3 +11,25 @@ class Categories(models.Model):
         db_table = "category"
         verbose_name = "category"
         verbose_name_plural = "categories"
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="goods_images", null=True, blank=True)
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    discount = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2
+    )
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(
+        Categories, on_delete=models.CASCADE, related_name="categories"
+    )
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "product"
+        verbose_name_plural = "products"
