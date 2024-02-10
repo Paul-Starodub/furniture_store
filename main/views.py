741ff13f5e06990b0db2ea86258d8b706acf46b3
Paul Starodub <amorallex@gmail.com>
2024-02-10 13:22:55 +0200

    update goods/views.py & base.html with data from db

--- main/views.py
+++ main/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    context = {"title": "Home - Main Page", "content": "FURNITURE STORE HOME"}
+    categories = Categories.objects.all()
+    context = {
+        "title": "Home - Main Page",
+        "content": "FURNITURE STORE HOME",
+        "categories": categories,
+    }
     return render(request, "main/index.html", context)
 
 
