cdcacb264b209046c1b55bf81f6219c5dc830e66
Paul Starodub <amorallex@gmail.com>
2024-02-10 17:30:20 +0200

    update goods/models.py, change migrations

--- goods/models.py
+++ goods/models.py
@@ -25,7 +25,7 @@ class Products(models.Model):
     image = models.ImageField(upload_to="goods_images", null=True, blank=True)
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
     discount = models.DecimalField(
-        default=0.00, max_digits=7, decimal_places=2
+        default=0.00, max_digits=4, decimal_places=2
     )
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(
