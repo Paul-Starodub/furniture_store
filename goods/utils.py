13911099b0a2c207439eb4a7bf2c4df09bca1e6d
Paul Starodub <amorallex@gmail.com>
2024-02-13 22:39:25 +0200

    update search with SearchRank

--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,8 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+)
 from django.db.models import QuerySet
 
 from goods.models import Products
@@ -8,6 +12,9 @@ def q_search(query: str) -> QuerySet:
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    return Products.objects.annotate(
-        search=SearchVector("name", "description")
-    ).filter(search=query)
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by(
+        "-rank"
+    )
