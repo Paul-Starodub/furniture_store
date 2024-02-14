d9cbdb60f552299f0d7252d3a35a77e81f6efa9d
Paul Starodub <amorallex@gmail.com>
2024-02-14 18:14:42 +0200

    add templates for user

--- users/views.py
+++ users/views.py
@@ -4,17 +4,17 @@ from django.shortcuts import render
 
 def login(request: HttpRequest) -> HttpResponse:
     context = {"title": "Home - Authorization"}
-    return render(request, "", context)
+    return render(request, "users/login.html", context)
 
 
 def registration(request: HttpRequest) -> HttpResponse:
     context = {"title": "Home - Registration"}
-    return render(request, "", context)
+    return render(request, "users/registration.html", context)
 
 
 def profile(request: HttpRequest) -> HttpResponse:
     context = {"title": "Home - Profile"}
-    return render(request, "", context)
+    return render(request, "users/profile.html", context)
 
 
 def logout(request: HttpRequest) -> HttpResponse:
