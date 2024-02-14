010bfe66e10dc33b088d0b9b49b38698ae87542b
Paul Starodub <amorallex@gmail.com>
2024-02-14 22:44:10 +0200

    customize forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,27 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "autofocus": True,
+                "class": "form-control",
+                "placeholder": "Enter your username",
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "current-password",
+                "class": "form-control",
+                "placeholder": "Enter your password",
+            }
+        ),
+    )
+
     class Meta:
         model = get_user_model()
