b6edcfb603867801e528714026dfcb30de10ced1
Paul Starodub <amorallex@gmail.com>
2024-02-14 22:14:36 +0200

    add users/forms.py,update views.py for fbv login

---
+++ users/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = get_user_model()
--- users/views.py
+++ users/views.py
@@ -1,9 +1,25 @@
-from django.http import HttpRequest, HttpResponse
+from django.contrib import auth
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 
 def login(request: HttpRequest) -> HttpResponse:
-    context = {"title": "Home - Authorization"}
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = auth.authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserLoginForm()
+    context = {"title": "Home - Authorization", "form": form}
     return render(request, "users/login.html", context)
 
 
