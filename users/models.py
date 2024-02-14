2e99854857cefe998da7c5911304193eaa1890b6
Paul Starodub <amorallex@gmail.com>
2024-02-14 20:34:18 +0200

    update users/models.py, add migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(
+        upload_to="users_images", blank=True, null=True, verbose_name="Avatar"
+    )
+
+    class Meta:
+        db_table = "user"
+
+    def __str__(self) -> str:
+        return self.username
