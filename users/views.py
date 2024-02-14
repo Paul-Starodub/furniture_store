ead6e21ecbd9c1ddcd85ff5459ce9bcd996210fc
Paul Starodub <amorallex@gmail.com>
2024-02-14 12:54:13 +0200

    prepare initial values in users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request: HttpRequest) -> HttpResponse:
+    context = {"title": "Home - Authorization"}
+    return render(request, "", context)
+
+
+def registration(request: HttpRequest) -> HttpResponse:
+    context = {"title": "Home - Registration"}
+    return render(request, "", context)
+
+
+def profile(request: HttpRequest) -> HttpResponse:
+    context = {"title": "Home - Profile"}
+    return render(request, "", context)
+
+
+def logout(request: HttpRequest) -> HttpResponse:
+    pass
