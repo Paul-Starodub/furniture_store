8c00d65b473ab3a51ffcfab8a42cf4fa5593cea8
Paul Starodub <amorallex@gmail.com>
2024-02-15 20:53:03 +0200

    update forms.py with ProfileForm and validation at the backend level

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserCreationForm,
+    UserChangeForm,
+)
 from django import forms
 
 
@@ -26,3 +30,40 @@ class UserRegistrationForm(UserCreationForm):
             "password1",
             "password2",
         )
+
+
+class ProfileForm(UserChangeForm):
+    image = forms.ImageField(
+        widget=forms.FileInput(attrs={"class": "form-control mt-3"}),
+        required=False,
+    )
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Enter your name"}
+        )
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter your last name",
+            }
+        )
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter your username",
+            }
+        )
+    )
+    email = forms.CharField(
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Enter your email"}
+        )
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ("image", "first_name", "last_name", "username", "email")
