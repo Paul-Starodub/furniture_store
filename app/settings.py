eb02d92ec3394c69fef078285031759bb5004209
Paul Starodub <amorallex@gmail.com>
2024-02-15 23:34:35 +0200

    add redirect to target page

--- app/settings.py
+++ app/settings.py
@@ -146,3 +146,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_URL = "user:login"
