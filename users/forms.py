7472f4ab1f56fbe2a3e37fa7b0bcf8c560273fd4
Paul Starodub <amorallex@gmail.com>
2024-02-15 14:49:21 +0200

    add some refactoring for login.html & forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,5 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
-from django import forms
 
 
 class UserLoginForm(AuthenticationForm):
     pass
-    username = forms.CharField()
-    password = forms.CharField()
-
-    class Meta:
-        model = get_user_model()
-        fields = ("username", "password")  # it isn't necessary here
