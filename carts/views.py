88db7a7937cfd9b39d06624dc34c0dd4ab84fe44
Paul Starodub <starodubpaul@gmail.com>
2024-02-28 07:48:13 +0200

    update views.py with cart_add

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,25 @@
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 
+from carts.models import Cart
+from goods.models import Products
 
-def cart_add(request, product_slug):
-    pass
+
+def cart_add(request: HttpRequest, product_slug: str):
+    product = Products.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product)
+
+    return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
 
 
 def cart_change(request, product_slug):
