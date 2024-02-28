17c58894ffd09406b685b2d02ff51f67ea8031b5
Paul Starodub <starodubpaul@gmail.com>
2024-02-28 07:27:11 +0200

    add migrations for cart, change product_id for product_slug

--- carts/urls.py
+++ carts/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from carts import views
 
 urlpatterns = [
-    path("cart_add/<int:product_id>/", views.cart_add, name="cart_add"),
+    path("cart_add/<int:product_slug>/", views.cart_add, name="cart_add"),
     path(
-        "cart_change/<int:product_id>/", views.cart_change, name="cart_change"
+        "cart_change/<int:product_slug>/",
+        views.cart_change,
+        name="cart_change",
     ),
     path(
-        "cart_remove/<int:product_id>/", views.cart_remove, name="cart_remove"
+        "cart_remove/<int:product_slug>/",
+        views.cart_remove,
+        name="cart_remove",
     ),
 ]
 
--- carts/views.py
+++ carts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 
-def cart_add(request, product_id):
+def cart_add(request, product_slug):
     pass
 
 
-def cart_change(request, product_id):
+def cart_change(request, product_slug):
     pass
 
 
-def cart_remove(request, product_id):
+def cart_remove(request, product_slug):
     pass
